cd87f49fccd57c14713498cbe4e3211044f6f3f6
Samantha Emerson <s.emerson@ufl.edu>
2020-03-04 11:40:34 -0500

    Add functional .js document to sort publications by date and store the 10 most recent in a user-specified .json file

--- src/recentpubs.js
+++ src/recentpubs.js
@@ -1,25 +1,72 @@
 // Most Recent Publications
-const tpf = require("./src/tpf")
-const entity = require("./src/entity")
+const tpf = require("./tpf")
+const entity = require("./entity")
 async function main() {
     const client = new tpf.Client("https://vivo.metabolomics.info/tpf/core")
     const publicationIRIs = await entity.Publications(client)
     const names = await entity.Names(client)
+    const authorships = await entity.Authorships(client)
+    const years = await entity.PublicationYears(client)
+
     // const names = await entity.Authorships(client)
+
+    // Build the authors mapping: author IRI => list of pubs.
+    const authors = {}
+
+    // authorships: IRI => list of unordered, pair (pub, person)
+    const authorshipIRIs = Object.keys(authorships)
+    for (var i = 0; i < authorshipIRIs.length; i++) {
+        const authorshipIRI = authorshipIRIs[i]
+        const relates = authorships[authorshipIRI]
+
+        // Unfortunately, the pub-person pair could be in either
+        // order. If the IRI contains "pmid", it's for the Pub.
+        var publicationIRI = relates[0]
+        var personIRI = relates[1]
+        if (relates[0].indexOf("pmid") === -1) {
+            personIRI = relates[0]
+            publicationIRI = relates[1]
+        }
+
+        if (!authors[publicationIRI]) {
+            authors[publicationIRI] = []
+        }
+
+        authors[publicationIRI].push(personIRI)
+
+    }
+
+    
+
     // TODO: get publication dates and sort by them
     const publications = []
-    for (const publicationIRI of publicationIRIs.slice(0, 10)) {
+    // arrays have a sort method (publicationIRIs.sort() and then pass a comparison function (use mozilla))
+    publicationIRIs.sort(function (a, b) {
+        const adate = years[a]
+        const bdate = years[b]
+        if (adate < bdate) {
+            return -1
+        }
+        if (adate > bdate) {
+            return +1
+        }
+        return 0
+    }).reverse()
+    for (const pubIRI of publicationIRIs.slice(0, 10)) {
+        var authornames = authors[pubIRI]
+            .map(function (authorIRI) {
+                return names[authorIRI]
+            })
         const publication = {
-            "iri": publicationIRI,
-            "pmid": "304850303",
-            "title": names[publicationIRI],
-            "authors": [
-                "Guoxiang Xie"
-            ],
-            "published": "2020-01-01T00:00:00"
+            "iri": pubIRI,
+            "pmid": pubIRI.slice(47,-1),
+            "title": names[pubIRI],
+            "authors": authornames,
+            "published": years[pubIRI]
         }
         publications.push(publication)
     }
     console.log(JSON.stringify(publications))
 }
+
 main()
